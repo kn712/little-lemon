d6269fedb0aab9b781d011108eaf3e5845616f46
Ning <ningke947@gmail.com>
2024-12-23 17:39:28 -0800

    12-23 commit

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Header(){
+    return <header>
+       <img src="/logo.jpg" className="img-fluid" alt="Logo" />
+    </header>;
+}
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nav(){
+    return <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html">Location</a></li>
+            <li><a href="index.html">Blog</a></li>
+        </ul>
+    </nav>;
+}
+export default Nav;
